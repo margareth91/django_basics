f395d8be80eb19d0e0c05dece5d5d64df5b7edcf
margareth91 <margerytka@wp.eu>
2023-11-02 20:45:44 +0100

    settings update

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import mimetypes
 from pathlib import Path
-import mimetypes
-
-mimetypes.add_type("text/css", ".css", True)
+# import mimetypes
+#
+# mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
