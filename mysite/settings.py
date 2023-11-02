9e9431548c96bb9864677a4c3dd3c9e13a6470e1
margareth91 <margerytka@wp.eu>
2023-11-02 20:18:32 +0100

    Revert "static root update"
    This reverts commit e19dfa4e2faf0bed8b9259c38fbe583a778f72d6.

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'static'
-STATIC_ROOT = "/home/malgo/malgo.pythonanywhere.com/static/css"
+STATIC_ROOT = "/home/malgo/malgo.pythonanywhere.com/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
