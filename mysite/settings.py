e19dfa4e2faf0bed8b9259c38fbe583a778f72d6
margareth91 <margerytka@wp.eu>
2023-11-02 20:17:03 +0100

    static root update

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'static'
-STATIC_ROOT = "/home/malgo/malgo.pythonanywhere.com/static"
+STATIC_ROOT = "/home/malgo/malgo.pythonanywhere.com/static/css"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
