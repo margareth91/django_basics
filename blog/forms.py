28a00fa591d5cabf85a279f4df18b4e658e1381b
margareth91 <margerytka@wp.eu>
2023-11-03 14:20:47 +0100

    views & templates update added login/logout

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
@@ -10,3 +10,8 @@ class PostForm(forms.ModelForm):
         fields = ('title', 'text')
 
 
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text')
